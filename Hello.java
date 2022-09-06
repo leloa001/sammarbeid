ff34de23717160c3b7a434af3dc0a82fa6b58bc6
Olemio <112165341+Olemio@users.noreply.github.com>
2022-09-06 14:08:43 +0200

    Hello again?

--- Hello.java
+++ Hello.java
@@ -2,7 +2,11 @@ public class Hello
 {
     public static void main(String[] args)
     {
-        System.out.println("Hello n0rd");
+        System.out.println("Hello world");
+        String helloWorld[] = {"H","e","l","l","o"};
+        for(int i = 0; i<helloWorld.length; i++) {
+            String str = helloWorld[i];
+            System.out.println(str);
+        }
     }
 }
-// test
